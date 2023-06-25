e7386def0961f08faa7277935c1df5dfb5873e49
Kauê de Moraes Vestena <kauemv2@gmail.com>
2023-06-25 03:02:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,37 +5,27 @@
 """
 
 # Full city name, it may contain special characters, spaces...
-CITY_NAME = 'Curitiba'
+CITY_NAME = 'Prizren'
 
 # simple name, spaces must be replaced by underscores, no special characters, all in lowercase
-CITY_SHORTNAME = 'curitiba'
+CITY_SHORTNAME = 'prizren'
 
 # username, for adresses
-USERNAME = 'kauevestena'
+USERNAME = 'opensidewalkmap'
 
 # repository name, for many weblink references:
-REPO_NAME = 'opensidewalkmap_beta'
+REPO_NAME = 'prizren'
 
 # City OSM relation id: (search at  https://nominatim.openstreetmap.org/ui/search.html ):
-CITY_RELATION_ID = 'R297514' #as string!!
+CITY_RELATION_ID = 'N150906142' #as string!!
 
 # BOUNDING BOXES
 # A good tool to find them is: bboxfinder.com
 # # entire city: 
-BOUNDING_BOX = (
-    -25.6450101000000004, # SOUTHMOST  LATITUDE
-    -49.3891399999999976, # WESTMOST   LONGITUDE
-    -25.3467008999999983, # NORTHMOST  LATITUDE
-    -49.1843181999999999  # EASTMOST   LONGITUDE
-    )
+BOUNDING_BOX = (20.678329,42.179434,20.802269,42.248089)
 
 # # A sample of a region of special interest, like the city centre, 
 # # It must have sidewalks as geometries and be inside the bigger one!!
-BOUNDING_BOX_SAMPLE = (
-    -25.46340831586,     # SOUTHMOST  LATITUDE
-    -49.26485433156466,  # WESTMOST   LONGITUDE
-    -25.45836407828201,  # NORTHMOST  LATITUDE
-    -49.257818266840495  # EASTMOST   LONGITUDE
-    )
+BOUNDING_BOX_SAMPLE = (20.699401,42.226960,20.727832,42.237383)
 
 STREAMLIT_URL='https://kauevestena-opensidewalkmap-beta-streamlit-routing-app-52bins.streamlitapp.com/'
